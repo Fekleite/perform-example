68a72e9e2722e4772e8dd65270c968bb7b42a557
Fernanda Leite <fernandacarolineleite360@gmail.com>
2021-05-18 22:02:42 -0300

    memo

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react"
+
 interface SearchResultsProps {
   product: {
     id: number;
@@ -6,10 +8,14 @@ interface SearchResultsProps {
   }
 }
 
-export function ProductItem({ product }: SearchResultsProps ) {
+function ProductItemComponent({ product }: SearchResultsProps ) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   )
 }
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.product, nextProps.product)
+})
